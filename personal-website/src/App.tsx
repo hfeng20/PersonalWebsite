a0c5a636dd711037a2fc88041eca00ffbc9bcb83
Harry Feng <fengharryt@gmail.com>
2022-05-12 22:08:55 -0400

    migrated from JS project

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import musician from './musician.JPG'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="MainPage">
+        <div className = "Bio">
+          <h1>
+            Harry Tianhong Feng
+          </h1>
+          <body>
+            Student
+            <br></br>
+            <br></br>
+            Athlete
+            <br></br>
+            <br></br>
+            Musician
+            <br></br>
+            <a href = '/music'>
+              <img className = "ImageLink" src = {musician} alt = "Musician Link Image" width="20%" height="10%" object-fit></img>
+            </a>
+            <br></br>
+            Programmer
+          </body>
+        </div>
     </div>
   );
 }
