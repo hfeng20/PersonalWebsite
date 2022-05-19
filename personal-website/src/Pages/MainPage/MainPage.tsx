0fb526a60fa1eba21f721e176106d39cafaea9d0
Harry Feng <fengharryt@gmail.com>
2022-05-18 22:16:37 -0400

    Cleaned up MainPage

--- personal-website/src/Pages/MainPage/MainPage.tsx
+++ personal-website/src/Pages/MainPage/MainPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import musician from './musician.JPG'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
+import './MainPage.css';
 import Activity from '../../Components/Activity/Activity';
 
 export default function mainPage(): JSX.Element {
